0f5b59a47f8d4fd2d04250879dff5b59ff6e2eaf
erick novoa <ericknovoa8@gmail.com>
2026-02-13 19:33:37 -0500

    agregue funciones de sumar y multiplicar

--- matematicas.js
+++ matematicas.js
@@ -1,5 +1,16 @@
-console.log ("Hola, este es el sistema matematico inicial");
+console.log ("Hola, este es el sistema matematico inicial ingrese \n 1. para sumar \n Ingrese 2 para mutiplicar"  );
+
+var opcion = prompt("Ingrese su opcion");
+
+    var num1 = parseFloat(prompt("Ingrese el primer número:"));
+    var num2 = parseFloat(prompt("Ingrese el segundo número:"));
 
 
-let opcion = prompt(" Ingrese 1 para sumar \n Ingrese 2 para mutiplicar ")
-let opcion = prompt(" Ingrese 1 para sumar \n Ingrese 2 para mutiplicar ")
+
+    if (opcion == "1") {
+        var resultado = num1 + num2;
+    }else if (opcion == "2") {
+        var resultado = num1 * num2;
+    }
+
+
