9dad3ad8df3893f742d709ac246c599187d2ca7e
erick novoa <ericknovoa8@gmail.com>
2026-02-13 19:30:32 -0500

    Se acomodo el codigo

--- matematicas.js
+++ matematicas.js
@@ -2,3 +2,4 @@ console.log ("Hola, este es el sistema matematico inicial");
 
 
 let opcion = prompt(" Ingrese 1 para sumar \n Ingrese 2 para mutiplicar ")
+let opcion = prompt(" Ingrese 1 para sumar \n Ingrese 2 para mutiplicar ")
